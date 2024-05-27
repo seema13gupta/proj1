0f4c8eb268b9fa069d47b7dfaa381d79aa5f4f9b
Seema Gupta <t-seemagupta@microsoft.com>
2024-05-27 21:42:36 +0530

    1st commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> INTODUCTION </h1>
+      
+        
+        <pr>Hy my self <b> Seema Gupta</b> i am in my 3rd year of my college from NIT DURGAPUR  currently i am working as associate consultant in mi </pr>
+        
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
